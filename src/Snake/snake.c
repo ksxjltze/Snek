11199ad9dc39f5095762ba9279e0373b3b89a636
ksxjltze <leejiakeat@gmail.com>
2020-10-18 19:32:12 +0800

    merge

--- src/Snake/snake.c
+++ src/Snake/snake.c
@@ -2,6 +2,7 @@
 #include "CProcessing.h"
 #include <stdio.h>
 #include <stdlib.h> //malloc
+#include <stdbool.h> //bool
 
 //Define in Snake.c
 static const int WINDOW_WIDTH = 1200, WINDOW_HEIGHT = 800;
@@ -11,6 +12,7 @@ float CELL_WIDTH;
 float CELL_HEIGHT;
 static float speed = 5; //Grids per second
 static CP_Vector WINDOW_CENTRE;
+int old_direction;
 
 enum direction
 {
@@ -32,6 +34,7 @@ struct Segment
 	struct Segment* next;
 	CP_Vector position;
 	int direction;
+	bool turning;
 };
 
 struct Snake
@@ -44,30 +47,46 @@ struct Snake
 }the_snake;
 
 void Snake_Init(void)
-{	
+{
 	CP_Image img_snake = CP_Image_Load("./Assets/snek.png");
 	WINDOW_CENTRE = CP_Vector_Set((float)WINDOW_WIDTH / 2, (float)WINDOW_HEIGHT / 2);
 	CELL_WIDTH = (float)WINDOW_WIDTH / GRID_SIZE;
 	CELL_HEIGHT = (float)WINDOW_HEIGHT / GRID_SIZE;
 
-	struct Segment* head = malloc(sizeof* head);
+	struct Segment* head = malloc(sizeof * head);
 	if (head == 0)
-	{
 		return;
-	}
+
+	struct Segment* current = malloc(sizeof * head);
+	if (current == 0)
+		return;
 
 	head->direction = RIGHT;
 	head->position = WINDOW_CENTRE;
-	head->next = NULL;
+	head->next = current;
+	the_snake.head = head;
+	old_direction = RIGHT;
 
 	the_snake.sprite.image = img_snake;
-	the_snake.head = head;
-	//the_snake.position = WINDOW_CENTRE;
-	//the_snake.direction = RIGHT;
 	the_snake.speed = (speed * (CELL_WIDTH + CELL_HEIGHT) / 2);
 
 	the_snake.sprite.width = (float)CP_Image_GetWidth(img_snake);
 	the_snake.sprite.height = (float)CP_Image_GetHeight(img_snake);
+
+	for (int i = 1; i <= 10; i++)
+	{
+		struct Segment* next = malloc(sizeof * head);
+		if (next == 0)
+			break;
+		next->next = NULL;
+
+		current->direction = RIGHT;
+		current->position = CP_Vector_Set(head->position.x - the_snake.sprite.width, head->position.y);
+		head = current;
+		current->next = next;
+		current = next;
+	}
+
 
 
 }
@@ -76,6 +95,7 @@ void Snake_Update(void)
 {
 	Snake_UpdateInput();
 	Snake_UpdateMovement();
+	Snake_UpdateDirection();
 	Snake_Draw();
 
 }
@@ -95,6 +115,7 @@ void Snake_Exit(void)
 
 void Snake_UpdateInput(void)
 {
+	old_direction = the_snake.head->direction;
 	if (CP_Input_KeyTriggered(KEY_W) || CP_Input_KeyTriggered(KEY_UP))
 	{
 		if (the_snake.head->direction != DOWN)
@@ -119,26 +140,82 @@ void Snake_UpdateInput(void)
 			the_snake.head->direction = RIGHT;
 	}
 
+	if (old_direction != the_snake.head->direction)
+		the_snake.head->turning = true;
+
+}
+
+CP_Vector Snake_CalculateMovement(int direction)
+{
+	CP_Vector movement_vector = CP_Vector_Set(0, 0);
+	if (direction % 2 == 0)
+		movement_vector = CP_Vector_Scale(
+			CP_Vector_Normalize(
+				CP_Vector_Set((float)direction, 0)), the_snake.speed * CP_System_GetDt()); //Horizontal
+	else
+		movement_vector = CP_Vector_Scale(
+			CP_Vector_Normalize(
+				CP_Vector_Set(0, (float)direction)), the_snake.speed * CP_System_GetDt()); //Vertical
+
+	return movement_vector;
+}
+
+void Snake_UpdateDirection(void)
+{
+	struct Segment* head = the_snake.head;
+	struct Segment* current = the_snake.head->next;
+
+	while (current->next != NULL)
+	{
+		if (head->turning)
+		{
+			current->direction = head->direction;
+		}
+
+		head = current;
+		current = current->next;
+
+	}
 }
 
 void Snake_UpdateMovement(void)
 {
-	CP_Vector movement_vector = CP_Vector_Set(0, 0);
+	the_snake.head->position = CP_Vector_Add(the_snake.head->position, Snake_CalculateMovement(the_snake.head->direction));
+	struct Segment* current = the_snake.head->next;
 
-	if (the_snake.head->direction % 2 == 0)
-		movement_vector = CP_Vector_Scale(
-			CP_Vector_Normalize(
-				CP_Vector_Set((float)the_snake.head->direction, 0)), the_snake.speed * CP_System_GetDt()); //Horizontal
-	else
-		movement_vector = CP_Vector_Scale(
-			CP_Vector_Normalize(
-				CP_Vector_Set(0, (float)the_snake.head->direction)), the_snake.speed * CP_System_GetDt()); //Vertical
+	while (current->next != NULL)
+	{
+		current->position = CP_Vector_Add(current->position, Snake_CalculateMovement(current->direction));
+		current = current->next;
 
-	the_snake.head->position = CP_Vector_Add(the_snake.head->position, movement_vector);
+	}
+
 }
+
 
 void Snake_Draw(void)
 {
+#if 0
+	for (int i = 0; i < GRID_SIZE; i++)
+	{
+		/*CP_Graphics_DrawLine(0, i * CELL_HEIGHT, (float)WINDOW_WIDTH, i * CELL_HEIGHT);
+		CP_Graphics_DrawLine(i * CELL_WIDTH, 0, i * CELL_WIDTH, (float)WINDOW_HEIGHT);*/
+		for (int j = 0; j < GRID_SIZE; j++)
+		{
+			CP_Graphics_DrawCircle(i * CELL_WIDTH + CELL_WIDTH / 2, j * CELL_HEIGHT + CELL_HEIGHT / 2, 3);
+
+		}
+	}
+#endif
+
 	CP_Settings_Background(CP_Color_Create(0, 0, 0, 255));
 	CP_Image_Draw(the_snake.sprite.image, the_snake.head->position.x, the_snake.head->position.y, the_snake.sprite.width, the_snake.sprite.height, 255);
+	struct Segment* current = the_snake.head->next;
+	while (current->next != NULL)
+	{
+		CP_Image_Draw(the_snake.sprite.image, current->position.x, current->position.y, the_snake.sprite.width, the_snake.sprite.height, 255);
+		current = current->next;
+
+	}
+
 }
--- src/Snake/snake.h
+++ src/Snake/snake.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "cprocessing.h"
 //Declare in Snake.h
 void Snake_Init(void);
 void Snake_Update(void);
@@ -6,4 +7,6 @@ void Snake_Exit(void);
 void Snake_UpdateInput(void);
 void Snake_UpdateMovement(void);
 void Snake_Draw(void);
+void Snake_UpdateDirection(void);
+CP_Vector Snake_CalculateMovement(int direction);
 
