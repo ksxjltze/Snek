3c73beb8bf0548db4638c41c294b2386a4382179
ksxjltze <leejiakeat@gmail.com>
2020-10-20 00:20:18 +0800

    Added functions to create buttons in MainMenu.c, created new struct Button and enum ButtonTypes

--- src/Snake/MainMenu.h
+++ src/Snake/MainMenu.h
@@ -24,9 +24,27 @@ struct Button2
 	float y;
 }exit_button;
 
+struct Button
+{
+	int type;
+
+	CP_Image image;
+	const char* text;
+
+	CP_Vector position;
+	float width;
+	float height;
+};
+
+enum Button_Types { BUTTON, TEXT_BUTTON, IMAGE_BUTTON };
+
 void Button_init(void);
 void Button_update(void);
 void Menu_init(void);
 void Menu_update(void);
 void Menu_exit(void);
+
+struct Button Create_Button(CP_Vector position, float width, float height);
+struct Button Create_TextButton(CP_Vector position, float width, float height, char* text);
+struct Button Create_ImageButton(CP_Vector position, float width, float height, CP_Image image);
 
