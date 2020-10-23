12ec452366194a08c41e9058eef1995624b4ebb1
Liu Ke <kerr.liu98@gmail.com>
2020-10-23 22:36:05 +0800

    kerr.liu98@gmail.com

---
+++ src/Snake/food.c
@@ -0,0 +1,41 @@
+#include "food.h"
+#include "sprite.h"
+#include "cprocessing.h"
+#include "grid.h"
+
+
+struct Food
+{
+	struct Sprite sprite;
+
+	CP_Vector position;						//Screen position (pixels)
+	int grid_position;						//Grid position (0 < position < GRID_SIZE)
+	int direction;
+}the_food;
+
+void food_Draw(void)
+{
+
+	//Draw Snake's head.
+	CP_Image_Draw(the_food.sprite.image,
+		the_food.position.x, the_food.position.y,
+		the_food.sprite.width, the_food.sprite.height, 255);
+};
+
+void init_food(void)
+{
+
+	CP_Image img_snake = CP_Image_Load("./Assets/food.png");
+	CP_Settings_Stroke(CP_Color_Create(255, 255, 255, 255)); //White lines
+
+	//food_Grid_Init();									//Initialize Grid specific variables
+
+	the_food.sprite.image = img_snake;
+	the_food.grid_position = 25;
+
+
+//	the_food.sprite.width = GRID_WIDTH;
+//	the_food.sprite.height = GRID_WIDTH;
+
+
+};
---
+++ src/Snake/food.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "cprocessing.h"
+#include <stdio.h>
+
+
+//Draw
+void food_Draw(void);
+
+void init_food(void);
