0cd52f5b4d5e51a53c099e30a8d8afb672a3636d
ksxjltze <leejiakeat@gmail.com>
2020-10-26 11:04:34 +0800

    r+ to r

--- src/Snake/file_manager.c
+++ src/Snake/file_manager.c
@@ -25,7 +25,7 @@ int ReadFile(void)
 	errno_t error;
 	int score = 0;
 
-	error = fopen_s(&fptr, "highscore.txt", "r+");
+	error = fopen_s(&fptr, "highscore.txt", "r");
 
 	if (error != 0)
 		return 0;
