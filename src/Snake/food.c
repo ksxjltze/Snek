25c5b4d704f2b862a57fcf185ed07c7aa955d1ae
ksxjltze <leejiakeat@gmail.com>
2020-10-26 14:42:22 +0800

    merge

--- src/Snake/food.c
+++ src/Snake/food.c
@@ -56,15 +56,11 @@ void food_update(CP_Vector grid[])
 void collision(CP_Vector position, CP_Vector grid[])
 {
 	if (position.x == the_food.position.x && position.y == the_food.position.y)
-<<<<<<< HEAD
 	{ 
 		score.total = score.total + 1;
 
 	//	the_food.eaten = 0;
 
-=======
-	{
->>>>>>> 492280cf3c11b066499fc00f23a6ea2de7219f58
 		the_food.grid_position = CP_Random_RangeInt(0, 324);     // food position generated inside the barrier
 		the_food.position = grid[the_food.grid_position]; //Screen Position
 		Food_score.count++;
